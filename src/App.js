1178761102af015dc642cf03fa806bf517128df2
thusharakart <thusharakart@gmail.com>
2021-06-09 22:36:58 +0530

    added sounds to App.js and index.css

--- src/App.js
+++ src/App.js
@@ -1,181 +1,210 @@
-import React, { useCallback, useRef, useState } from 'react';
-import Crossword from '@jaredreisinger/react-crossword';
-import styled from 'styled-components';
+import React, { useCallback, useRef, useState } from "react";
+import { Crossword, ThemeProvider } from "@jaredreisinger/react-crossword";
+import styled from "styled-components";
+
+import "./App.css";
+
+let correctSound;
 
 const data = {
-  across: {
-    1: {
-      clue: 'one plus one',
-      answer: 'TWO',
-      row: 0,
-      col: 0,
-    },
-    
-  },
+	across: {
+		1: {
+			clue: "one plus one",
+			answer: "TWO",
+			row: 0,
+			col: 0,
+		},
+	},
 
-  down: {
-    2: {
-      clue: 'three minus two',
-      answer: 'ONE',
-      row: 0,
-      col: 2,
-    },
-    1: {
-      clue: 'five plus five',
-      answer: 'TENET',
-      row: 0,
-      col: 0,
-    }
-    
-  },
+	down: {
+		2: {
+			clue: "three minus two",
+			answer: "ONE",
+			row: 0,
+			col: 2,
+		},
+		1: {
+			clue: "five plus five",
+			answer: "TENET",
+			row: 0,
+			col: 0,
+		},
+	},
 };
 
 const Page = styled.div`
-  padding: 2em;
+	padding: 2em;
 `;
 
 const Header = styled.h1`
-  margin-bottom: 1em;
+	margin-bottom: 1em;
 `;
 
 const Commands = styled.div``;
 
 const Command = styled.button`
-  margin-right: 1em;
+	margin-right: 1em;
 `;
 
 const CrosswordWrapper = styled.div`
-  margin-top: 2em;
-  max-width: 30em;
-  /* and some fun making use of the defined class names */
-  .crossword.correct {
-    rect {
-      stroke: rgb(100, 200, 100) !important;
-    }
-    svg > rect {
-      fill: rgb(100, 200, 100) !important;
-    }
-    text {
-      fill: rgb(100, 200, 100) !important;
-    }
-  }
-  .clue.correct {
-    ::before {
-      content: '\u2713'; /* a.k.a. checkmark: ✓ */
-      display: inline-block;
-      text-decoration: none;
-      color: rgb(100, 200, 100);
-      margin-right: 0.25em;
-    }
-    text-decoration: line-through;
-    color: rgb(130, 130, 130);
-  }
+	margin-top: 2em;
+	max-width: 30em;
+	/* and some fun making use of the defined class names */
+	.crossword.correct {
+		rect {
+			stroke: rgb(100, 200, 100) !important;
+		}
+		svg > rect {
+			fill: rgb(100, 200, 100) !important;
+		}
+		text {
+			fill: rgb(100, 200, 100) !important;
+		}
+	}
+	.clue.correct {
+		::before {
+			content: "\u2713"; /* a.k.a. checkmark: ✓ */
+			display: inline-block;
+			text-decoration: none;
+			color: rgb(100, 200, 100);
+			margin-right: 0.25em;
+		}
+		text-decoration: line-through;
+		color: rgb(130, 130, 130);
+	}
 `;
 
 const Messages = styled.pre`
-  background-color: rgb(230, 230, 230);
-  margin: 1em 0;
-  padding: 1em;
+	background-color: rgb(230, 230, 230);
+	margin: 1em 0;
+	padding: 1em;
 `;
 
 // in order to make this a more-comprehensive example, and to vet Crossword's
 // features, we actually implement a fair amount...
 
 function App() {
-  const crossword = useRef();
+	const crossword = useRef();
 
-  const focus = useCallback((event) => {
-    crossword.current.focus();
-  }, []);
+	const focus = useCallback((event) => {
+		crossword.current.focus();
+	}, []);
 
-  const fillAllAnswers = useCallback((event) => {
-    crossword.current.fillAllAnswers();
-  }, []);
+	const fillAllAnswers = useCallback((event) => {
+		crossword.current.fillAllAnswers();
+	}, []);
 
-  const reset = useCallback((event) => {
-    crossword.current.reset();
-  }, []);
+	const reset = useCallback((event) => {
+		crossword.current.reset();
+	}, []);
 
-  // We don't really *do* anything with callbacks from the Crossword component,
-  // but we can at least show that they are happening.  You would want to do
-  // something more interesting than simply collecting them as messages.
-  const [messages, setMessages] = useState([]);
+	// We don't really *do* anything with callbacks from the Crossword component,
+	// but we can at least show that they are happening.  You would want to do
+	// something more interesting than simply collecting them as messages.
+	const [messages, setMessages] = useState([]);
 
-  const addMessage = useCallback((message) => {
-    setMessages((m) => m.concat(`${message}\n`));
-  }, []);
+	const addMessage = useCallback((message) => {
+		setMessages((m) => m.concat(`${message}\n`));
+	}, []);
 
-  // onCorrect is called with the direction, number, and the correct answer.
-  const onCorrect = useCallback(
-    (direction, number, answer) => {
-      addMessage(`onCorrect: "${direction}", "${number}", "${answer}"`);
-    },
-    [addMessage]
-  );
+	function sound(src) {
+		this.sound = document.createElement("audio");
+		this.sound.src = src;
+		this.sound.setAttribute("preload", "auto");
+		this.sound.setAttribute("controls", "none");
+		this.sound.style.display = "none";
+		document.body.appendChild(this.sound);
+		this.play = function () {
+			this.sound.play();
+		};
+		this.stop = function () {
+			this.sound.pause();
+		};
+	}
 
-  // onLoadedCorrect is called with an array of the already-correct answers,
-  // each element itself is an array with the same values as in onCorrect: the
-  // direction, number, and the correct answer.
-  const onLoadedCorrect = useCallback(
-    (answers) => {
-      addMessage(
-        `onLoadedCorrect:\n${answers
-          .map(
-            ([direction, number, answer]) =>
-              `    - "${direction}", "${number}", "${answer}"`
-          )
-          .join('\n')}`
-      );
-    },
-    [addMessage]
-  );
+	// onCorrect is called with the direction, number, and the correct answer.
+	const onCorrect = useCallback(
+		(direction, number, answer) => {
+			addMessage(`onCorrect: "${direction}", "${number}", "${answer}"`);
 
-  // onCrosswordCorrect is called with a truthy/falsy value.
-  const onCrosswordCorrect = useCallback(
-    (isCorrect) => {
-      addMessage(`onCrosswordCorrect: ${JSON.stringify(isCorrect)}`);
-    },
-    [addMessage]
-  );
+			correctSound = new sound(
+				"https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+			);
+			correctSound.play();
+		},
+		[addMessage],
+	);
 
-  // onCellChange is called with the row, column, and character.
-  const onCellChange = useCallback(
-    (row, col, char) => {
-      addMessage(`onCellChange: "${row}", "${col}", "${char}"`);
-    },
-    [addMessage]
-  );
+	// onLoadedCorrect is called with an array of the already-correct answers,
+	// each element itself is an array with the same values as in onCorrect: the
+	// direction, number, and the correct answer.
+	const onLoadedCorrect = useCallback(
+		(answers) => {
+			addMessage(
+				`onLoadedCorrect:\n${answers
+					.map(
+						([direction, number, answer]) =>
+							`    - "${direction}", "${number}", "${answer}"`,
+					)
+					.join("\n")}`,
+			);
+		},
+		[addMessage],
+	);
 
-  return (
-    <Page>
-      <Header>@jaredreisinger/react-crossword example app</Header>
+	// onCrosswordCorrect is called with a truthy/falsy value.
+	const onCrosswordCorrect = useCallback(
+		(isCorrect) => {
+			addMessage(`onCrosswordCorrect: ${JSON.stringify(isCorrect)}`);
+		},
+		[addMessage],
+	);
 
-      <p>
-        This is a demo app that makes use of the @jaredreisinger/react-crossword
-        component. It excersizes most of the functionality, so that you can see
-        how to do so.
-      </p>
+	// onCellChange is called with the row, column, and character.
+	const onCellChange = useCallback(
+		(row, col, char) => {
+			addMessage(`onCellChange: "${row}", "${col}", "${char}"`);
+		},
+		[addMessage],
+	);
 
-      <Commands>
-        <Command onClick={focus}>Focus</Command>
-        <Command onClick={fillAllAnswers}>Fill all answers</Command>
-        <Command onClick={reset}>Reset</Command>
-      </Commands>
+	return (
+		<div class="damsy">
+			<Page>
+				<Header>SparkX Crossword</Header>
 
-      <CrosswordWrapper>
-        <Crossword
-          data={data}
-          ref={crossword}
-          onCorrect={onCorrect}
-          onLoadedCorrect={onLoadedCorrect}
-          onCrosswordCorrect={onCrosswordCorrect}
-          onCellChange={onCellChange}
-        />
-      </CrosswordWrapper>
+				<Commands>
+					<Command onClick={focus}>Focus</Command>
+					<Command onClick={fillAllAnswers}>Fill all answers</Command>
+					<Command onClick={reset}>Reset</Command>
+				</Commands>
 
-      <Messages>{messages}</Messages>
-    </Page>
-  );
+				<CrosswordWrapper>
+					<Crossword
+						data={data}
+						theme={{
+							columnBreakpoint: "9999px",
+							gridBackground: "transparent",
+							cellBackground: "transparent",
+							cellBorder: "#000",
+							textColor: "#fff",
+							numberColor: "#425",
+							focusBackground: "#b48",
+							highlightBackground: "#aaa",
+						}}
+						ref={crossword}
+						onCorrect={onCorrect}
+						onLoadedCorrect={onLoadedCorrect}
+						onCrosswordCorrect={onCrosswordCorrect}
+						onCellChange={onCellChange}
+					/>
+				</CrosswordWrapper>
+
+				{/* 
+        <Messages>{messages}</Messages> */}
+			</Page>
+		</div>
+	);
 }
 
 export default App;
