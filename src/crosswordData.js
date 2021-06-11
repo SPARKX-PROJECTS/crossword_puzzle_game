834febe17a337fb54c1a20d7ce4e2c83df7a12e9
DamsyDeSilva <damsydesilva@gmail.com>
2021-06-12 03:55:51 +0530

    crossword data added

---
+++ src/crosswordData.js
@@ -0,0 +1,192 @@
+const data_1 = {
+  across: {
+    1: {
+      clue: "JANUARY",
+      answer: "DURUTHU",
+      row: 0,
+      col: 6,
+    },
+    3: {
+      clue: "MAY",
+      answer: "VESAK",
+      row: 4,
+      col: 7,
+    },
+    5: {
+      clue: "APRIL",
+      answer: "BAK",
+      row: 6,
+      col: 9,
+    },
+    6: {
+      clue: "OCTOBER",
+      answer: "VAP",
+      row: 6,
+      col: 5,
+    },
+    8: {
+      clue: "JUNE",
+      answer: "POSON",
+      row: 4,
+      col: 1,
+    },
+    9: {
+      clue: "SEPTEMBER",
+      answer: "BINARA",
+      row: 7,
+      col: 0,
+    },
+    11: {
+      clue: "MARCH",
+      answer: "MADIN",
+      row: 8,
+      col: 5,
+    },
+  },
+
+  down: {
+    2: {
+      clue: "DECEMBER",
+      answer: "UNDUVAP",
+      row: 0,
+      col: 7,
+    },
+    4: {
+      clue: "JULY",
+      answer: "ESALA",
+      row: 2,
+      col: 10,
+    },
+    7: {
+      clue: "FEBRUARY",
+      answer: "NAVAM",
+      row: 4,
+      col: 5,
+    },
+    10: {
+      clue: "AUGUST",
+      answer: "NIKINI",
+      row: 7,
+      col: 2,
+    },
+    12: {
+      clue: "NOVEMBER",
+      answer: "ILL",
+      row: 8,
+      col: 8,
+    },
+  },
+};
+
+const data_2 = {
+  across: {
+    1: {
+      clue: "Original name",
+      answer: "SIDDHARTHA",
+      row: 4,
+      col: 2,
+    },
+    5: {
+      clue: "Born Cast",
+      answer: "SHAKYA",
+      row: 9,
+      col: 6,
+    },
+    6: {
+      clue: "Horse name",
+      answer: "KANTHAKA",
+      row: 12,
+      col: 0,
+    },
+    7: {
+      clue: "Mother's Name",
+      answer: "MAHAMAYA",
+      row: 7,
+      col: 9,
+    },
+  },
+  down: {
+    2: {
+      clue: "Born City",
+      answer: "LUMBINI",
+      row: 0,
+      col: 3,
+    },
+    4: {
+      clue: "Son's Name",
+      answer: "RAHULA",
+      row: 2,
+      col: 10,
+    },
+    3: {
+      clue: "Was Married to",
+      answer: "YASHODHARA",
+      row: 3,
+      col: 7,
+    },
+    8: {
+      clue: "Father's Name",
+      answer: "SUDDODHANA",
+      row: 5,
+      col: 1,
+    },
+  },
+};
+
+const data_3 = {
+  across: {
+    1: {
+      clue: "Bought to Sri Lanka by theri Sangamitta",
+      answer: "SRIMAHABODHIYA",
+      row: 4,
+      col: 3,
+    },
+    4: {
+      clue: "This was a nine story building and built by king Dutugemunu",
+      answer: "LOVAMAHAPAYA",
+      row: 11,
+      col: 4,
+    },
+    5: {
+      clue: "Second Highest Stupa in the country",
+      answer: "ABHAYAGIRIYA",
+      row: 14,
+      col: 4,
+    },
+    6: {
+      clue: "A stupa built by king Mahasena",
+      answer: "JETHAVANARAMAYA",
+      row: 6,
+      col: 6,
+    },
+    7: {
+      clue: "The stupa built by king Dutugemunu after defeating king Elara",
+      answer: "MIRISAVETIYA",
+      row: 9,
+      col: 6,
+    },
+    8: {
+        clue: "A stuoa built by king Vattagamini Abaya",
+        answer: "LANKARAMAYA",
+        row: 16,
+        col: 0,
+      },
+  },
+  down: {
+    2: {
+      clue: "A marvel stupa built by king Dutugemunu",
+      answer: "RUWANWELISAYA",
+      row: 4,
+      col: 4,
+    },
+    3: {
+      clue: "Fist stupa built in Anuradhapura",
+      answer: "THUPARAMAYA",
+      row: 0,
+      col: 16,
+    },
+  },
+};
+
+let header_array = ["POYA DAYS", "BUDDHA'S EARLY LIFE", "EIGHT SACRED PLACES IN ANURADHAPURA"];
+let data_array = [data_1, data_2, data_3];
