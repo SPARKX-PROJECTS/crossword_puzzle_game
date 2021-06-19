1466b9f33559613c51396016be6dd4f0fcbda240
panduka <kandyman777@gmail.com>
2021-06-19 11:36:11 +0530

    feat( load next puzzle): added load next puzzle option

--- src/index/GameIndex.js
+++ src/index/GameIndex.js
@@ -1,43 +1,44 @@
-import React, { useState } from "react";
-import ChooseScreen from "../choosePuzzleScreen/ChooseScreen";
-import GameStartScreen from "../startscreen/GameStartScreen";
-import App from "../App";
+import React, { useState } from 'react';
+import ChooseScreen from '../choosePuzzleScreen/ChooseScreen';
+import GameStartScreen from '../startscreen/GameStartScreen';
+import App from '../App';
 
 function GameIndex() {
-	const [screenState, setScreenState] = useState("START_SCREEN");
-	const [puzzleId, setPuzzleId] = useState(0);
-	const [soundOn, setSoundOn] = useState(true);
+  const [screenState, setScreenState] = useState('START_SCREEN');
+  const [puzzleId, setPuzzleId] = useState(0);
+  const [soundOn, setSoundOn] = useState(true);
 
-	if (screenState === "START_SCREEN") {
-		//setScreenState( "CHOOSE_SCREEN");
-		return (
-			<GameStartScreen
-				setScreenState={setScreenState}
-				setSoundOn={setSoundOn}
-				soundOn={soundOn}
-			/>
-		);
-	} else if (screenState === "CHOOSE_SCREEN") {
-		//setScreenState( "START_SCREEN");
-		return (
-			<ChooseScreen
-				setScreenState={setScreenState}
-				setPuzzleId={setPuzzleId}
-				setSoundOn={setSoundOn}
-				soundOn={soundOn}
-			/>
-		);
-	} else if (screenState === "PUZZLE_SCREEN") {
-		//setScreenState( "CHOOSE_SCREEN");
-		return (
-			<App
-				setScreenState={setScreenState}
-				puzzleId={puzzleId}
-				setSoundOn={setSoundOn}
-				soundOn={soundOn}
-			/>
-		);
-	}
+  if (screenState === 'START_SCREEN') {
+    //setScreenState( "CHOOSE_SCREEN");
+    return (
+      <GameStartScreen
+        setScreenState={setScreenState}
+        setSoundOn={setSoundOn}
+        soundOn={soundOn}
+      />
+    );
+  } else if (screenState === 'CHOOSE_SCREEN') {
+    //setScreenState( "START_SCREEN");
+    return (
+      <ChooseScreen
+        setScreenState={setScreenState}
+        setPuzzleId={setPuzzleId}
+        setSoundOn={setSoundOn}
+        soundOn={soundOn}
+      />
+    );
+  } else if (screenState === 'PUZZLE_SCREEN') {
+    //setScreenState( "CHOOSE_SCREEN");
+    return (
+      <App
+        setScreenState={setScreenState}
+        puzzleId={puzzleId}
+        setPuzzleId={setPuzzleId}
+        setSoundOn={setSoundOn}
+        soundOn={soundOn}
+      />
+    );
+  }
 }
 
 export default GameIndex;
