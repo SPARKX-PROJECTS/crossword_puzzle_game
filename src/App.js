614e86b10ed91edd9021ec2da0712bdf8a158f29
thusharakart <thusharakart@gmail.com>
2021-06-20 02:12:51 +0530

    fix(fillAllAnswers): removed popup from fill all answers;

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
-import React, { useCallback, useRef, useState, useEffect } from 'react';
-import Crossword from './Crossword';
-import styled from 'styled-components';
-import './App.css';
-import './Popup/Popup.css';
-import { puzzleDataStore } from './crosswordData';
+import React, { useCallback, useRef, useState, useEffect } from "react";
+import Crossword from "./Crossword";
+import styled from "styled-components";
+import "./App.css";
+import "./Popup/Popup.css";
+import { puzzleDataStore } from "./crosswordData";
 
-import { AwesomeButton } from 'react-awesome-button';
-import 'react-awesome-button/dist/styles.css';
+import { AwesomeButton } from "react-awesome-button";
+import "react-awesome-button/dist/styles.css";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReply } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReply } from "@fortawesome/free-solid-svg-icons";
 
 //modal for popup
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
 
 let correctSound = new sound(
-  'https://crossword-dpfiyymcf-sparkx-projects.vercel.app/audio/correct.mp3'
+	"https://crossword-dpfiyymcf-sparkx-projects.vercel.app/audio/correct.mp3",
 );
 let resetSound = new sound(
-  'https://crossword-dpfiyymcf-sparkx-projects.vercel.app/audio/reset.wav'
+	"https://crossword-dpfiyymcf-sparkx-projects.vercel.app/audio/reset.wav",
 );
 let focusSound = new sound(
-  'https://crossword-dpfiyymcf-sparkx-projects.vercel.app/audio/focus.wav'
+	"https://crossword-dpfiyymcf-sparkx-projects.vercel.app/audio/focus.wav",
 );
 let allCorrectSounnd = new sound(
-  'https://crossword-dpfiyymcf-sparkx-projects.vercel.app/audio/complete.wav'
+	"https://crossword-dpfiyymcf-sparkx-projects.vercel.app/audio/complete.wav",
 );
 
 const Page = styled.div`
-  padding: 2em;
-  padding-top: 0rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  flex-wrap: nowrap;
-  background-repeat: no-repeat;
+	padding: 2em;
+	padding-top: 0rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	flex-wrap: nowrap;
+	background-repeat: no-repeat;
 `;
 
 const CrosswordWrapper = styled.div`
@@ -69,237 +69,241 @@ const CrosswordWrapper = styled.div`
 `;
 
 function sound(src) {
-  this.sound = document.createElement('audio');
-  this.sound.src = src;
-  this.sound.setAttribute('preload', 'auto');
-  this.sound.setAttribute('controls', 'none');
-  this.sound.style.display = 'none';
-  document.body.appendChild(this.sound);
-  this.play = function () {
-    this.sound.play();
-  };
-  this.stop = function () {
-    this.sound.pause();
-  };
+	this.sound = document.createElement("audio");
+	this.sound.src = src;
+	this.sound.setAttribute("preload", "auto");
+	this.sound.setAttribute("controls", "none");
+	this.sound.style.display = "none";
+	document.body.appendChild(this.sound);
+	this.play = function () {
+		this.sound.play();
+	};
+	this.stop = function () {
+		this.sound.pause();
+	};
 }
 
 // in order to make this a more-comprehensive example, and to vet Crossword's
 // features, we actually implement a fair amount...
 function App({ puzzleId, setScreenState, setSoundOn, soundOn, setPuzzleId }) {
-  const crossword = useRef();
-  const [popupOpen, setPopupOpen] = React.useState(false);
+	const crossword = useRef();
+	const [popupOpen, setPopupOpen] = useState(false);
+	const [fillAllClicked, setFillAllClicked] = useState(false);
 
-  //this style class is for the modal
-  const useStyles = makeStyles((theme) => ({
-    modal: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  }));
+	//this style class is for the modal
+	const useStyles = makeStyles((theme) => ({
+		modal: {
+			display: "flex",
+			alignItems: "center",
+			justifyContent: "center",
+		},
+	}));
 
-  const classes = useStyles();
-  //========>
+	const classes = useStyles();
 
-  useEffect(() => {
-    crossword.current.reset();
-  });
+	useEffect(() => {
+		crossword.current.reset();
+	});
 
-  const focus = useCallback(
-    (event) => {
-      crossword.current.focus();
-      if (!soundOn) {
-        focusSound.play();
-      }
-    },
-    [soundOn]
-  );
+	const focus = useCallback(
+		(event) => {
+			crossword.current.focus();
+			if (!soundOn) {
+				focusSound.play();
+			}
+		},
+		[soundOn],
+	);
 
-  const fillAllAnswers = useCallback(
-    (event) => {
-      crossword.current.fillAllAnswers();
-      if (!soundOn) {
-        allCorrectSounnd.play();
-      }
-    },
-    [soundOn]
-  );
+	const fillAllAnswers = useCallback(
+		(event) => {
+			setFillAllClicked(true);
+			crossword.current.fillAllAnswers();
+			if (!soundOn) {
+				allCorrectSounnd.play();
+			}
+		},
+		[soundOn],
+	);
 
-  const reset = useCallback(
-    (event) => {
-      crossword.current.reset();
-      if (!soundOn) {
-        resetSound.play();
-      }
-    },
-    [soundOn]
-  );
+	const reset = useCallback(
+		(event) => {
+			//crossword.current.reset();
+			//setFillAllClicked(false);
+			if (!soundOn) {
+				resetSound.play();
+			}
+		},
+		[soundOn],
+	);
 
-  // We don't really *do* anything with callbacks from the Crossword component,
-  // but we can at least show that they are happening.  You would want to do
-  // something more interesting than simply collecting them as messages.
-  // const [messages, setMessages] = useState([]);
-  // const addMessage = useCallback((message) => {
-  //   setMessages((m) => m.concat(`${message}\n`));
-  // }, []);
+	// We don't really *do* anything with callbacks from the Crossword component,
+	// but we can at least show that they are happening.  You would want to do
+	// something more interesting than simply collecting them as messages.
+	// const [messages, setMessages] = useState([]);
+	// const addMessage = useCallback((message) => {
+	//   setMessages((m) => m.concat(`${message}\n`));
+	// }, []);
 
-  // onCorrect is called with the direction, number, and the correct answer.
-  const onCorrect = useCallback(
-    (direction, number, answer) => {
-      if (!soundOn) {
-        correctSound.play();
-      }
-    },
-    [soundOn]
-  );
+	// onCorrect is called with the direction, number, and the correct answer.
+	const onCorrect = useCallback(
+		(direction, number, answer) => {
+			if (!soundOn) {
+				correctSound.play();
+			}
+		},
+		[soundOn],
+	);
 
-  // onLoadedCorrect is called with an array of the already-correct answers,
-  // each element itself is an array with the same values as in onCorrect: the
-  // direction, number, and the correct answer.
-  const onLoadedCorrect = useCallback((answers) => {}, []);
+	// onLoadedCorrect is called with an array of the already-correct answers,
+	// each element itself is an array with the same values as in onCorrect: the
+	// direction, number, and the correct answer.
+	const onLoadedCorrect = useCallback((answers) => {}, []);
 
-  // onCrosswordCorrect is called with a truthy/falsy value.
-  const onCrosswordCorrect = useCallback(
-    (isCorrect) => {
-      if (isCorrect) {
-        handleOpen();
-        //setIsPopUpOpen(true);
-        if (!soundOn) {
-          allCorrectSounnd.play();
-        }
-      }
-    },
-    [soundOn]
-  );
+	// onCrosswordCorrect is called with a truthy/falsy value.
+	const onCrosswordCorrect = useCallback(
+		(isCorrect) => {
+			if (isCorrect) {
+				if (!fillAllClicked) {
+					handleOpen();
+				}
+				//setIsPopUpOpen(true);
+				if (!soundOn) {
+					allCorrectSounnd.play();
+				}
+			}
+		},
+		[soundOn, fillAllClicked],
+	);
 
-  // onCellChange is called with the row, column, and character.
-  const onCellChange = useCallback((row, col, char) => {}, []);
+	// onCellChange is called with the row, column, and character.
+	const onCellChange = useCallback((row, col, char) => {}, []);
 
-  const handleOpen = () => {
-    setPopupOpen(true);
-  };
+	const handleOpen = () => {
+		setPopupOpen(true);
+	};
 
-  const handleClose = () => {
-    setPopupOpen(false);
-  };
+	const handleClose = () => {
+		setPopupOpen(false);
+	};
 
-  const goBack = () => {
-    setScreenState('CHOOSE_SCREEN');
-  };
+	const goBack = () => {
+		setScreenState("CHOOSE_SCREEN");
+	};
 
-  const replay = () => {
-    handleClose();
-    setScreenState('PUZZLE_SCREEN');
-  };
+	const replay = () => {
+		handleClose();
+		setScreenState("PUZZLE_SCREEN");
+	};
 
-  const loadNextPuzzle = () => {
-    handleClose();
-    setPuzzleId(puzzleDataStore.length - 1 > puzzleId ? puzzleId + 1 : 0);
-  };
+	const loadNextPuzzle = () => {
+		handleClose();
+		setPuzzleId(puzzleDataStore.length - 1 > puzzleId ? puzzleId + 1 : 0);
+	};
 
-  return (
-    <>
-      <div class="content">
-        <Page>
-          <Modal
-            aria-labelledby="transition-modal-title"
-            aria-describedby="transition-modal-description"
-            open={popupOpen}
-            onClose={handleClose}
-            className={classes.modal}
-            closeAfterTransition
-            BackdropComponent={Backdrop}
-            BackdropProps={{
-              timeout: 500,
-            }}
-          >
-            <Fade in={popupOpen}>
-              <div className="popup_box">
-                <div className="popup_box_button_panel">
-                  <AwesomeButton
-                    className="popup_box_button"
-                    ripple
-                    type="primary"
-                    onPress={goBack}
-                    size="small"
-                  >
-                    New Puzzle
-                  </AwesomeButton>
+	return (
+		<>
+			<div class="content">
+				<Page>
+					<Modal
+						aria-labelledby="transition-modal-title"
+						aria-describedby="transition-modal-description"
+						open={popupOpen}
+						onClose={handleClose}
+						className={classes.modal}
+						closeAfterTransition
+						BackdropComponent={Backdrop}
+						BackdropProps={{
+							timeout: 500,
+						}}
+					>
+						<Fade in={popupOpen}>
+							<div className="popup_box">
+								<div className="popup_box_button_panel">
+									<AwesomeButton
+										className="popup_box_button"
+										ripple
+										type="primary"
+										onPress={goBack}
+										size="small"
+									>
+										New Puzzle
+									</AwesomeButton>
 
-                  <AwesomeButton
-                    className="popup_box_button"
-                    ripple
-                    type="primary"
-                    onPress={loadNextPuzzle}
-                    size="small"
-                  >
-                    Next puzzle
-                  </AwesomeButton>
+									<AwesomeButton
+										className="popup_box_button"
+										ripple
+										type="primary"
+										onPress={loadNextPuzzle}
+										size="small"
+									>
+										Next puzzle
+									</AwesomeButton>
 
-                  <AwesomeButton
-                    className="popup_box_button"
-                    ripple
-                    type="primary"
-                    onPress={replay}
-                    size="small"
-                  >
-                    Replay
-                  </AwesomeButton>
-                </div>
-              </div>
-            </Fade>
-          </Modal>
+									<AwesomeButton
+										className="popup_box_button"
+										ripple
+										type="primary"
+										onPress={replay}
+										size="small"
+									>
+										Replay
+									</AwesomeButton>
+								</div>
+							</div>
+						</Fade>
+					</Modal>
 
-          <div className="div_game_category_title">
-            <AwesomeButton size="icon" ripple type="primary" onPress={goBack}>
-              <FontAwesomeIcon icon={faReply} />
-            </AwesomeButton>
+					<div className="div_game_category_title">
+						<AwesomeButton size="icon" ripple type="primary" onPress={goBack}>
+							<FontAwesomeIcon icon={faReply} />
+						</AwesomeButton>
 
-            {/* <h3 className="game_category_title">CATEGORY : {header}</h3> */}
-          </div>
+						{/* <h3 className="game_category_title">CATEGORY : {header}</h3> */}
+					</div>
 
-          <div className="div_game_button_panel">
-            <AwesomeButton ripple type="secondary" onPress={focus}>
-              Focus
-            </AwesomeButton>
+					<div className="div_game_button_panel">
+						<AwesomeButton ripple type="secondary" onPress={focus}>
+							Focus
+						</AwesomeButton>
 
-            <AwesomeButton ripple type="secondary" onPress={reset}>
-              {/* <Sound
+						<AwesomeButton ripple type="secondary" onPress={reset}>
+							{/* <Sound
               url="http://localhost:3000/audio/correct.mp3"
               playStatus={Sound.status.PLAYING}
             /> */}
-              Reset
-            </AwesomeButton>
+							Reset
+						</AwesomeButton>
 
-            <AwesomeButton ripple type="secondary" onPress={fillAllAnswers}>
-              Fill Answers
-            </AwesomeButton>
-          </div>
+						<AwesomeButton ripple type="secondary" onPress={fillAllAnswers}>
+							Fill Answers
+						</AwesomeButton>
+					</div>
 
-          <div>
-            <CrosswordWrapper>
-              <Crossword
-                data={puzzleDataStore[puzzleId].data}
-                theme={{
-                  numberColor: 'rgb(0,0,0)',
-                  cellBackground: 'transparent',
-                  focusBackground: 'transparent',
-                  highlightBackground: 'rgb(45,125,250)',
-                  backgroundImage:
-                    'https://digitalsynopsis.com/wp-content/uploads/2017/03/beautiful-color-gradients-backgrounds-078-cochiti-lake.png',
-                }}
-                ref={crossword}
-                onCorrect={onCorrect}
-                onLoadedCorrect={onLoadedCorrect}
-                onCrosswordCorrect={onCrosswordCorrect}
-                onCellChange={onCellChange}
-              />
-            </CrosswordWrapper>
-          </div>
-        </Page>
-      </div>
-    </>
-  );
+					<div>
+						<CrosswordWrapper>
+							<Crossword
+								data={puzzleDataStore[puzzleId].data}
+								theme={{
+									numberColor: "rgb(0,0,0)",
+									cellBackground: "transparent",
+									focusBackground: "transparent",
+									highlightBackground: "rgb(45,125,250)",
+									backgroundImage:
+										"https://digitalsynopsis.com/wp-content/uploads/2017/03/beautiful-color-gradients-backgrounds-078-cochiti-lake.png",
+								}}
+								ref={crossword}
+								onCorrect={onCorrect}
+								onLoadedCorrect={onLoadedCorrect}
+								onCrosswordCorrect={onCrosswordCorrect}
+								onCellChange={onCellChange}
+							/>
+						</CrosswordWrapper>
+					</div>
+				</Page>
+			</div>
+		</>
+	);
 }
 
 export default App;
