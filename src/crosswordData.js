0e293b5bffa4aed674c858672acc276a8b36d186
DamsyDeSilva <damsydesilva@gmail.com>
2021-06-24 10:28:34 +0530

    fix( crosswordData): fixed the crossword data

--- src/crosswordData.js
+++ src/crosswordData.js
@@ -87,13 +87,13 @@ const data_2 = {
       col: 2,
     },
     5: {
-      clue: 'Born Cast',
+      clue: 'Cast born to',
       answer: 'SHAKYA',
       row: 9,
       col: 6,
     },
     6: {
-      clue: 'Horse name',
+      clue: 'Name of personal horse',
       answer: 'KANTHAKA',
       row: 12,
       col: 0,
@@ -107,7 +107,7 @@ const data_2 = {
   },
   down: {
     2: {
-      clue: 'Born City',
+      clue: 'Name of the Birth Place',
       answer: 'LUMBINI',
       row: 0,
       col: 3,
@@ -119,7 +119,7 @@ const data_2 = {
       col: 10,
     },
     3: {
-      clue: 'Was Married to',
+      clue: 'Name of Wife',
       answer: 'YASHODHARA',
       row: 3,
       col: 7,
@@ -136,37 +136,37 @@ const data_2 = {
 const data_3 = {
   across: {
     1: {
-      clue: 'Bought to Sri Lanka by theri Sangamitta',
+      clue: 'This was Brought to Sri Lanka by Sangamitta Theri',
       answer: 'SRIMAHABODHIYA',
       row: 4,
       col: 0,
     },
     4: {
-      clue: 'This was a nine story building and built by king Dutugemunu',
+      clue: 'This was a nine story building built by King Dutugemunu',
       answer: 'LOVAMAHAPAYA',
       row: 11,
       col: 1,
     },
     5: {
-      clue: 'Second Highest Stupa in the country',
+      clue: 'Second Highest Stupa in Sri Lanaka',
       answer: 'ABHAYAGIRIYA',
       row: 14,
       col: 1,
     },
     6: {
-      clue: 'A stupa built by king Mahasena',
+      clue: 'A stupa built by King Mahasena',
       answer: 'JETHAVANARAMAYA',
       row: 6,
       col: 3,
     },
     7: {
-      clue: 'The stupa built by king Dutugemunu after defeating king Elara',
+      clue: 'The stupa built by King Dutugemunu after defeating king Elara',
       answer: 'MIRISAVETIYA',
       row: 9,
       col: 3,
     },
     8: {
-      clue: 'A stupa built by king Vattagamini Abaya',
+      clue: 'A stupa built by King Vattagamini Abaya',
       answer: 'LANKARAMAYA',
       row: 16,
       col: 0,
@@ -174,13 +174,13 @@ const data_3 = {
   },
   down: {
     2: {
-      clue: 'A marvel stupa built by king Dutugemunu',
+      clue: 'A stupa that is considered a marvel built by King Dutugemunu',
       answer: 'RUWANWELISAYA',
       row: 4,
       col: 1,
     },
     3: {
-      clue: 'Fist stupa built in Anuradhapura',
+      clue: 'First stupa built in Anuradhapura',
       answer: 'THUPARAMAYA',
       row: 0,
       col: 13,
@@ -191,8 +191,8 @@ const data_3 = {
 const data_4 = {
   across: {
     2: {
-      clue: 'The place where the first arrival Buddhism happened in Sri Lanaka',
-      answer: 'MIHINTHALAWA',
+      clue: 'The inauguration of Buddhism in Sri lanka occured in this place',
+      answer: 'MIHINTALE',
       row: 2,
       col: 2,
     },
@@ -205,19 +205,19 @@ const data_4 = {
   },
   down: {
     1: {
-      clue: '_______ Maha Rahath Thero introduced Buddhism to Sri Lanka',
+      clue: '_______ Maha Rahath Thero inaugurated Buddhism to Sri Lanka',
       answer: 'MIHINDU',
       row: 0,
       col: 4,
     },
     4: {
-      clue: 'On this Poya day, arrival of buddhism in Sri Lanka Happened',
+      clue: 'On this Poya day, buddhism inaugurated Buddhism to Sri Lanka',
       answer: 'POSON',
       row: 4,
       col: 7,
     },
     5: {
-      clue: 'Who was the emperor in India, took initiatives to send buddhism to Sri Lanka',
+      clue: 'The Indian emperor who initiated the spread of Buddhism to Sri Lanka and South Asia',
       answer: 'ASHOKA',
       row: 4,
       col: 10,
@@ -235,21 +235,21 @@ let puzzleDataStore = [
   },
   {
     key: 1,
-    name: "BUDDHA'S EARLY LIFE",
+    name: "LORD BUDDHA'S EARLY LIFE",
     data: data_2,
     completed: false,
     time: '',
   },
   {
     key: 2,
-    name: 'EIGHT SACRED PLACES',
+    name: 'SACRED PLACES IN SRI LANKA',
     data: data_3,
     completed: false,
     time: '',
   },
   {
     key: 3,
-    name: 'ARRIVAL OF BUDDHISM IN SRI LANKA',
+    name: 'INAGURATION OF BUDDHISM IN SRI LANKA',
     data: data_4,
     completed: false,
     time: '',
