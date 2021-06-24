755275508b3ba4f2d64f4c8ac28cda4151bacc74
thusharakart <thusharakart@gmail.com>
2021-06-24 16:25:25 +0530

    removed(button) : removed the non-functional buttons; fix(link): add the links to social media; styles(screens): added the background color;

--- src/startscreen/GameStartScreen.js
+++ src/startscreen/GameStartScreen.js
@@ -1,128 +1,134 @@
-import React, { backHandler } from 'react';
+import React, { backHandler } from "react";
 
 import {
-  AwesomeButton,
-  AwesomeButtonProgress,
-  AwesomeButtonSocial,
-} from 'react-awesome-button';
-import 'react-awesome-button/dist/styles.css';
+	AwesomeButton,
+	AwesomeButtonProgress,
+	AwesomeButtonSocial,
+} from "react-awesome-button";
+import "react-awesome-button/dist/styles.css";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import {
-  faVolumeUp,
-  faVolumeMute,
-  faScrewdriver,
-  faGamepad,
-  faExclamationCircle,
-} from '@fortawesome/free-solid-svg-icons';
+	faVolumeUp,
+	faVolumeMute,
+	faScrewdriver,
+	faGamepad,
+	faExclamationCircle,
+} from "@fortawesome/free-solid-svg-icons";
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
-import './gamestartscreen.css';
+import "./gamestartscreen.css";
 
 function GameStartScreen(props) {
-  const soundControl = (event) => {
-    props.setSoundOn(!props.soundOn);
-  };
+	const soundControl = (event) => {
+		props.setSoundOn(!props.soundOn);
+	};
 
-  const startGame = () => {
-    props.setScreenState('CHOOSE_SCREEN');
-  };
+	const startGame = () => {
+		props.setScreenState("CHOOSE_SCREEN");
+	};
 
-  const quitGame = () => {};
+	const quitGame = () => {};
 
-  return (
-    <>
-      <motion.div
-        className="div_main_start_screen"
-        initial={{ opacity: 0.5 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0.5 }}
-      >
-        <div className="div_util_button_set">
-          <AwesomeButton size="icon" type="primary">
+	return (
+		<>
+			<motion.div
+				className="div_main_start_screen"
+				initial={{ opacity: 0.5 }}
+				animate={{ opacity: 1 }}
+				exit={{ opacity: 0.5 }}
+			>
+				<div className="div_util_button_set">
+					{/* <AwesomeButton size="icon" type="primary">
             <FontAwesomeIcon icon={faScrewdriver} />
-          </AwesomeButton>
+          </AwesomeButton> */}
 
-          <AwesomeButton
-            size="icon"
-            ripple
-            type="primary"
-            onPress={soundControl}
-          >
-            {props.soundOn ? (
-              <FontAwesomeIcon icon={faVolumeMute} />
-            ) : (
-              <FontAwesomeIcon icon={faVolumeUp} />
-            )}
-          </AwesomeButton>
-        </div>
+					<AwesomeButton
+						size="icon"
+						ripple
+						type="primary"
+						onPress={soundControl}
+					>
+						{props.soundOn ? (
+							<FontAwesomeIcon icon={faVolumeMute} />
+						) : (
+							<FontAwesomeIcon icon={faVolumeUp} />
+						)}
+					</AwesomeButton>
+				</div>
 
-        <div className="div_main_button_set">
-          <table>
-            <tr>
-              <td align="center">
-                <img
-                  className="crosswordLogo"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShKLM389KHNOwgyQk9ttreG_7Fidhbbc_IQA&usqp=CAU"
-                  alt=""
-                />
-              </td>
-            </tr>
-            <tr>
-              <td className="mainButtons">
-                <AwesomeButtonProgress
-                  type="secondary"
-                  onPress={startGame}
-                  fakePress="true"
-                  loadingLabel="Loading..."
-                >
-                  <h1>PLAY</h1>
-                </AwesomeButtonProgress>
-              </td>
-            </tr>
-            <tr>
-              <td className="mainButtons">
-                <AwesomeButton type="primary" onPress={quitGame}>
-                  <h1>QUIT</h1>
-                </AwesomeButton>
-              </td>
-            </tr>
-          </table>
-        </div>
+				<div className="div_main_button_set">
+					<table>
+						<tr>
+							<td align="center">
+								<img
+									className="crosswordLogo"
+									src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShKLM389KHNOwgyQk9ttreG_7Fidhbbc_IQA&usqp=CAU"
+									alt=""
+								/>
+							</td>
+						</tr>
+						<tr>
+							<td className="mainButtons">
+								<AwesomeButtonProgress
+									type="secondary"
+									onPress={startGame}
+									fakePress="true"
+									loadingLabel="Loading..."
+								>
+									<h1>PLAY</h1>
+								</AwesomeButtonProgress>
+							</td>
+						</tr>
+						<tr>
+							<td className="mainButtons">
+								{/* <AwesomeButton type="primary" onPress={quitGame}>
+									<h1>QUIT</h1>
+								</AwesomeButton> */}
+							</td>
+						</tr>
+					</table>
+				</div>
 
-        <div className="div_secondary_button_set">
-          <table>
-            <tr>
-              <td>
-                <img
-                  className="sparkLogo"
-                  src="https://avatars.githubusercontent.com/u/85065925?s=200&v=4"
-                  alt=""
-                />
-              </td>
-              <td className="tag_line">Spark Academy Sri Lanka</td>
-              <td>
-                <AwesomeButtonSocial
-                  className="socialMediaIcons"
-                  size="small"
-                  type="facebook"
-                />
-              </td>
-              <td>
-                <AwesomeButtonSocial
-                  className="socialMediaIcons"
-                  size="small"
-                  type="instagram"
-                />
-              </td>
-            </tr>
-          </table>
-        </div>
-      </motion.div>
-    </>
-  );
+				<div className="div_secondary_button_set">
+					<table>
+						<tr>
+							<td>
+								<a href="https://spark.lk" rel="noreferrer" target="_blank">
+									<img
+										className="sparkLogo"
+										src="https://avatars.githubusercontent.com/u/85065925?s=200&v=4"
+										alt=""
+									/>
+								</a>
+							</td>
+							<td className="tag_line">Spark Academy Sri Lanka</td>
+							<td>
+								<AwesomeButtonSocial
+									className="socialMediaIcons"
+									size="small"
+									type="facebook"
+									href="https://www.facebook.com/myspark.lk/"
+									target="_blank"
+								/>
+							</td>
+							<td>
+								<AwesomeButtonSocial
+									className="socialMediaIcons"
+									size="small"
+									type="instagram"
+									href="https://instagram.com/myspark.lk?utm_medium=copy_link"
+									target="_blank"
+								/>
+							</td>
+						</tr>
+					</table>
+				</div>
+			</motion.div>
+		</>
+	);
 }
 
 export default GameStartScreen;
